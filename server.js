2c3acb6e8d05ff8ba4de4974901e87f3e0464d09
Thomas Moore <tsanm93@gmail.com>
2015-12-28 21:31:08 +0000

    Current Game webpage nearly done

--- server.js
+++ server.js
@@ -1,7 +1,45 @@
+var Config = require('./config');
+
+var lolapi = require('lolapi')(Config.league.apikey, Config.league.location);
+var _ = require('underscore');
+var EventEmitter = require('events');
+var moment = require('moment');
+var gameMode = require('./modules/lol/util/gametype-constant');
+
 var express = require('express');
 var app = express();
 
 app.use(express.static('web'));
+
+app.get('/api/lol/current-game', function(req, res) {
+    var summonerID = req.query.summonerID;
+    lolapi.CurrentGame.getBySummonerId(summonerID, function(error, game) {
+        if (error) {
+            res.status(404).json({error: 'No Game Found'});
+            console.log(error);
+        } else {
+            game.gameType = gameMode(game);
+            game.gameLength = moment(game.gameStartTime).toNow(true);
+            
+            var getChampions = new EventEmitter();
+            var count = (game.participants.length - 1);
+            _.each(game.participants, function(participant, i) {
+                lolapi.Static.getChampion(participant.championId, function(error, champion) {
+                    game.participants[i].champion = champion.name;
+                    // Only send the json when we've set all the champions
+                    if (count === i) {
+                        getChampions.emit('completed', game);
+                    }
+                });
+            });
+            
+            getChampions.on('completed', function(game) {
+                _.groupBy(game.participants, 'teamId');
+                res.json(game);
+            });
+        }
+    });
+});
 
 var server = app.listen(5000, function () {
     var host = server.address().address;
