f6a6c3433212d29dd66def922f26575d3daeee65
Thomas Moore <tsanm93@gmail.com>
2015-12-28 16:01:14 +0000

    Fix url to commands

--- modules/help.js
+++ modules/help.js
@@ -2,7 +2,7 @@ var help = function(bot, channelID, message) {
     if (message === "!help") {
         bot.sendMessage({
             to: channelID,
-            message: "You can find my command list here: http://disbot.pagu.co/commands"
+            message: "You can find my command list here: http://disbot.pagu.co/#commands"
         });
     }
 }
