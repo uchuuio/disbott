2aa999d8d7e53a13e7f335963b535f4135736446
Thomas Moore <tsanm93@gmail.com>
2015-12-29 23:22:01 +0000

    Use the Config.domain

--- modules/help.js
+++ modules/help.js
@@ -2,7 +2,7 @@ var help = function(Config, bot, channelID, message) {
     if (message === "!help") {
         bot.sendMessage({
             to: channelID,
-            message: "You can find my command list here: http://disbot.pagu.co/#commands"
+            message: "You can find my command list here: " + Config.domain + "/#commands"
         });
     }
 }
