2f7298aa305aab7d331891eec57344daa0dd62be
Thomas Moore <tsanm93@gmail.com>
2016-01-02 01:28:29 +0000

    It's a channel not a server

--- modules/sound/modules/disconnect.js
+++ modules/sound/modules/disconnect.js
@@ -2,7 +2,7 @@ var disconnect = function(soundDb, bot, channelID, message, voiceChannelID) {
     if (message === "sounddisconnect") {
         bot.sendMessage({
             to: channelID,
-            message: "Disconnecting from Voice Server"
+            message: "Disconnecting from Voice Channel"
         }, function() {
             bot.leaveVoiceChannel(voiceChannelID);
             soundDb.remove({ enabled: true }, { multi: true }, function (err, numRemoved) {});
