64eca60ad1c3dd5201e12ddcf8b22669df66fc29
Thomas Moore <tsanm93@gmail.com>
2016-01-05 21:05:32 +0000

    Only look for channels that match on the current server

--- modules/management/modules/create-voice-invite.js
+++ modules/management/modules/create-voice-invite.js
@@ -2,24 +2,28 @@ var createVoiceInvite = function (_, S, bot, channelID, message) {
     if (S(message).contains("createvoiceinvite=")) {
         var splitMessage = message.split('=');
         var inviteChannel = splitMessage[1];
+        
+        var currentServerID = bot.serverFromChannel(channelID);
 
         _.each(bot.servers, function (server) {
-            _.each(server.channels, function (channel) {
-                if (channel.type === 'voice' && channel.name === inviteChannel) {
-                    var expiresIn = (60*60)*6; // 6hours
-                    bot.createInvite({
-                        channel: channel.id,
-                        max_age: expiresIn,
-                        temporary: true
-                    }, function(res) {
-                        bot.sendMessages(channelID, [
-                            "The following code/link lasts for 6hours",
-                            "The instant invite code is " + res.code + ".",
-                            "And the instant invite link is http://discord.gg/" + res.code
-                        ])
-                    });
-                }
-            });
+            if (server.id === currentServerID) {
+                _.each(server.channels, function (channel) {
+                    if (channel.type === 'voice' && channel.name === inviteChannel) {
+                        var expiresIn = (60*60)*6; // 6hours
+                        bot.createInvite({
+                            channel: channel.id,
+                            max_age: expiresIn,
+                            temporary: true
+                        }, function(res) {
+                            bot.sendMessages(channelID, [
+                                "The following code/link lasts for 6hours",
+                                "The instant invite code is " + res.code + ".",
+                                "And the instant invite link is http://discord.gg/" + res.code
+                            ])
+                        });
+                    }
+                });
+            }
         });
 
     }
