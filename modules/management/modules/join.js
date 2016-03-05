b9004fbd9f6552884df8aa98ad107c9b14748de1
Thomas Moore <tsanm93@gmail.com>
2016-03-05 12:30:29 +0000

    Give a helpful error message when trying to join a server

--- modules/management/modules/join.js
+++ modules/management/modules/join.js
@@ -3,11 +3,19 @@ var join = function (S, bot, channelID, message) {
 		var splitMessage = message.split('=');
 		var code = splitMessage[1];
 
-		bot.acceptInvite(code, function (res) {
-			bot.sendMessage({
-				to: channelID,
-				message: 'Joining channel: ' + res.channel.name + ' on ' + res.guild.name,
-			});
+		bot.acceptInvite(code, function (err, res) {
+			console.log(err, res);
+			if (err) {
+				bot.sendMessage({
+					to: channelID,
+					message: err + '. Did you mean `sound=' + code + '`?',
+				});
+			} else {
+				bot.sendMessage({
+					to: channelID,
+					message: 'Joining channel: ' + res.channel.name + ' on ' + res.guild.name,
+				});
+			}
 		});
 	}
 };
