d2d639136acd02cff4cf2b56d164cbfd231157d1
Thomas Moore <tomo@pagu.co>
2016-03-19 19:56:42 +0000

    Actually ping the chunder king

--- modules/silly/chunder.js
+++ modules/silly/chunder.js
@@ -2,7 +2,7 @@ var chunder = function (bot, channelID, message) {
 	if (message === 'who is the chunder king?') {
 		bot.sendMessage({
 			to: channelID,
-			message: '@tomopagu is the chunder king!',
+			message: '<@105753535659921408> is the chunder king!',
 		});
 	}
 };
