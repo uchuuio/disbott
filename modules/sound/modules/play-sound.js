c903f3afca52f37674e0819bb28f39c591a49a8b
Thomas Moore <tomo@pagu.co>
2016-04-23 21:18:01 +0100

    Make the sounds work

--- modules/sound/modules/play-sound.js
+++ modules/sound/modules/play-sound.js
@@ -1,16 +1,60 @@
-const fs = require('fs');
-const path = require('path');
-const childProcess = require('child_process');
+// const fs = require('fs');
 import S from 'string';
 const s = S;
 
-export default function playSound(client, e, message, stopPlaying) {
+export default function playSound(client, e, message) {
+	const guild = e.message.channel.guild;
+
+	function stop() {
+		const info = client.VoiceConnections.getForGuild(guild);
+		if (info) {
+			const encoderStream = info.voiceConnection.getEncoderStream();
+			encoderStream.unpipeAll();
+		}
+	}
+
+	function play(info, song) {
+		let newInfo = info;
+		if (!client.VoiceConnections.length) {
+			return console.log('Voice not connected'); // eslint-disable-line
+		}
+
+		if (!info) newInfo = client.VoiceConnections[0];
+
+		const encoder = newInfo.voiceConnection.createExternalEncoder({
+			type: 'ffmpeg',
+			source: `./modules/sound/sounds/${song}.mp3`,
+			frameDuration: 60,
+			format: 'opus',
+			inputArgs: [],
+			outputArgs: [],
+			debug: true,
+		});
+		if (!encoder) return console.log('Voice connection is no longer valid'); // eslint-disable-line
+
+		encoder.once('end', () => {
+			e.message.channel.sendMessage(`Finished playing ${song}`);
+		});
+
+		const encoderStream = encoder.play();
+		encoderStream.resetTimestamp();
+		encoderStream.removeAllListeners('timestamp');
+		// encoderStream.on('timestamp', time => console.log(`${time} time`)); // eslint-disable-line
+		return true;
+	}
+
+
 	if (s(message).contains('vplay=') || message === 'vstop' || message === 'vleave') {
 		const splitMessage = message.split('=');
 		const song = splitMessage[1];
 
 		if (s(message).contains('vplay=')) {
+			if (!client.VoiceConnections.length) {
+				return e.message.reply('Not connected to any channel');
+			}
+			const info = client.VoiceConnections.getForGuild(guild);
 			e.message.channel.sendMessage(`Attempting to play ${song}`);
+			if (info) play(info, song);
 		} else if (message === 'vleave') {
 			stop();
 
@@ -27,115 +71,7 @@ export default function playSound(client, e, message, stopPlaying) {
 			stop();
 			e.message.channel.sendMessage('Stopping...');
 		}
-
-		play();
 	}
 
-	function getConverter(args, options) {
-		const binaries = [
-			'ffmpeg',
-			'ffmpeg.exe',
-			'avconv',
-			'avconv.exe',
-		];
-
-		const paths = process.env.PATH.split(path.delimiter).concat(['.']);
-
-		for (const name of binaries) {
-			for (const p of paths) {
-				const binary = p + path.sep + name;
-				if (!fs.existsSync(binary)) continue;
-				return childProcess.spawn(name, args, options);
-			}
-		}
-
-		return null;
-	}
-
-	let ffmpeg = null;
-	function stop() {
-		stopPlaying = true;
-		if (!ffmpeg) return;
-		ffmpeg.kill();
-		ffmpeg = null;
-	}
-
-	function play(voiceConnectionInfo) {
-		stopPlaying = false;
-
-		const sampleRate = 48000;
-		const bitDepth = 16;
-		const channels = 1;
-
-		if (ffmpeg) ffmpeg.kill();
-
-		ffmpeg = getConverter([
-			'-re',
-			'-i', 'elvis.mp3',
-			// '-i', song + '.mp3',
-			'-f', 's16le',
-			'-ar', sampleRate,
-			'-ac', channels,
-			'pipe:1',
-		], { stdio: ['pipe', 'pipe', 'ignore'] });
-		if (!ffmpeg) return console.log('ffmpeg/avconv not found'); // eslint-disable-line
-
-		const ffmpegB = ffmpeg;
-		const ff = ffmpeg.stdout;
-
-		// note: discordie encoder does resampling if rate != 48000
-		const options = {
-			frameDuration: 60,
-			sampleRate,
-			channels,
-			float: false,
-		};
-
-		const frameDuration = 60;
-
-		const readSize =
-		sampleRate / 1000 *
-		options.frameDuration *
-		bitDepth / 8 *
-		channels;
-
-		ff.once('readable', () => {
-			if (!client.VoiceConnections.length) {
-				return console.log('Voice not connected'); // eslint-disable-line
-			}
-
-			if (!voiceConnectionInfo) {
-				// get first if not specified
-				voiceConnectionInfo = client.VoiceConnections[0];
-			}
-
-			const voiceConnection = voiceConnectionInfo.voiceConnection;
-
-			// 一 encoder per voice connection
-			const encoder = voiceConnection.getEncoder(options);
-
-			const needBuffer = () => encoder.onNeedBuffer();
-			encoder.onNeedBuffer = () => {
-				const chunk = ff.read(readSize);
-				console.log(ff); // eslint-disable-line
-
-				if (ffmpegB.killed) return;
-				if (stopPlaying) return stop();
-
-				// delay the packet if no data buffered
-				if (!chunk) return setTimeout(needBuffer, options.frameDuration);
-
-				const sampleCount = readSize / channels / (bitDepth / 8);
-				encoder.enqueue(chunk, sampleCount);
-			};
-
-			needBuffer();
-		});
-
-		ff.once('end', () => {
-			if (stopPlaying) return;
-			// setTimeout(play, 100, voiceConnectionInfo);
-			e.message.channel.sendMessage(`Finished playing ${song}`);
-		});
-	}
+	return true;
 }
