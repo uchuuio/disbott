cecc00c6fab84d8c21cf56d25a99e05dc0abe735
Thomas Moore <tomo@pagu.co>
2016-04-23 23:28:53 +0100

    Update to es2015

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 import { Config } from './config';
 
-var fs = require('fs');
+const fs = require('fs');
 
-var lolapi = require('lolapi')(Config.league.apikey, Config.league.location);
+const lolapi = require('lolapi')(Config.league.apikey, Config.league.location);
 import _ from 'underscore';
-var EventEmitter = require('events');
+const EventEmitter = require('events');
 import moment from 'moment';
 import gameMode from './modules/lol/modules/util/gametype-constant';
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.use(express.static('web'));
 
-app.get('/api/lol/current-game', function (req, res) {
-	var summonerID = req.query.summonerID;
-	lolapi.CurrentGame.getBySummonerId(summonerID, function (error, game) {
+app.get('/api/lol/current-game', (req, res) => {
+	const summonerID = req.query.summonerID;
+	lolapi.CurrentGame.getBySummonerId(summonerID, (error, currentGameInfo) => {
 		if (error) {
 			res.status(404).json({ error: 'No Game Found' });
-			console.log(error);
 		} else {
+			const game = currentGameInfo;
 			game.gameType = gameMode(game);
 			game.gameLength = moment(game.gameStartTime).toNow(true);
 
-			var getChampions = new EventEmitter();
-			var count = (game.participants.length - 1);
-			_.each(game.participants, function (participant, i) {
-				lolapi.Static.getChampion(participant.championId, function (error, champion) {
+			const getChampions = new EventEmitter();
+			const count = (game.participants.length - 1);
+			_.each(game.participants, (participant, i) => {
+				lolapi.Static.getChampion(participant.championId, (err, champion) => {
 					game.participants[i].champion = champion.name;
 
 					// Only send the json when we've set all the champions
@@ -36,24 +36,24 @@ app.get('/api/lol/current-game', function (req, res) {
 				});
 			});
 
-			getChampions.on('completed', function (game) {
-				_.groupBy(game.participants, 'teamId');
-				res.json(game);
+			getChampions.on('completed', (currentGame) => {
+				_.groupBy(currentGame.participants, 'teamId');
+				res.json(currentGame);
 			});
 		}
 	});
 });
 
-app.get('/api/sounds', function (req, res) {
-	fs.readdir('./modules/sound/sounds/', function (err, files) {
-		var sounds = _.without(files, '.DS_Store', '.gitignore');
+app.get('/api/sounds', (req, res) => {
+	fs.readdir('./modules/sound/sounds/', (err, files) => {
+		const sounds = _.without(files, '.DS_Store', '.gitignore', 'ENV');
 		res.json(sounds);
 	});
 });
 
-var server = app.listen(5000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
+const server = app.listen(5000, () => {
+	const host = server.address().address;
+	const port = server.address().port;
 
-	console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Disbott Web listening at http://%s:%s', host, port); // eslint-disable-line
 });
