bc6429ff80a9f991cc60dde2dd688a02b61a06e1
Thomas Moore <tomo@pagu.co>
2016-09-04 19:20:11 +0100

    Initial C# Tests

---
+++ Disbott3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Discord;
+using Disbott.Modules;
+
+class Program
+{
+    static void Main(string[] args) => new Program().Start();
+
+    private DiscordClient _client;
+
+    public void Start()
+    {
+        _client = new DiscordClient();
+
+        _client.MessageReceived += async (s, e) =>
+        {
+            Console.WriteLine(e.Message.Text);
+            if (!e.Message.IsAuthor && e.Message.IsMentioningMe())
+            {
+                var botUsername = e.Server.CurrentUser.Name;
+                var message = e.Message.Text.Replace("@"+botUsername+" ", "");
+                try
+                {
+                    await utils.Ping(e, message);
+                }
+
+                catch
+                {
+
+                }
+            }
+             // await e.Channel.SendMessage(e.Message.Text);
+        };
+
+        _client.ExecuteAndWait(async () => {
+            await _client.Connect("MTMxMDk0MzY0NjEyMDY3MzI4.Cq3bPQ.9lg-kJXG609wmychtrZAjK3sjYc");
+        });
+    }
+}
