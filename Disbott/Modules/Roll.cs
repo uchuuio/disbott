421ce833dd519c25d1ef208b953a5d81e0c80412
Samuel Rule <samuel.rule@reedonline.co.uk>
2016-09-16 16:23:01 +0100

    Actaully Commiting not like an idot
    Basic Diceroll

---
+++ Disbott/Modules/Roll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace Disbott.Modules
+{
+    [Module]
+    public class Roll
+    {
+        [Command("roll"), Description("Rolls a Dice!")]
+        public async Task d6(IUserMessage msg)
+        {
+            Random rnd = new Random();
+            int roll = rnd.Next(1, 7);
+            string result = roll.ToString();
+            await msg.Channel.SendMessageAsync(result);
+        }
+    }
+}
