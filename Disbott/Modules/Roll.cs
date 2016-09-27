181429225dbb7d6784e9901330b5afdb4c2fff5b
80cmdude <s80cmdude@gmail.com>
2016-09-27 22:10:13 +0100

    Finished First build of Dice roller
    First build complete. Loads of refactoring to go but main functionality
    is there.

--- Disbott/Modules/Roll.cs
+++ Disbott/Modules/Roll.cs
@@ -18,6 +18,7 @@ namespace Disbott.Modules
         [Command("roll"), Description("Rolls a Dice!")]
         public async Task roll(IUserMessage msg, string userInput)
         {
+            var DiscordID = msg.Author.Username;
             string dice = userInput;
             string pattern = "([1-9]\\d*)?d([1-9]\\d*)([/x][1-9]\\d*)?([+-]\\d+)?";
             string numberOfDice = "^.*?(?=d)";
@@ -28,28 +29,39 @@ namespace Disbott.Modules
             Match numberOfSidesPresent = Regex.Match(dice, numbeOfSides);
             if (m.Success)
             {
-                Console.WriteLine(m.Value);
-                Console.WriteLine(numberOfDiceRolled.Value);
-                Console.WriteLine(numberOfSidesPresent.Value);
-
                 int numberOfDiceRolledInt = Convert.ToInt32(numberOfDiceRolled.Value);
                 int numberOfSidesPresentInt = Convert.ToInt32(numberOfSidesPresent.Value);
 
-                Random rnd = new Random(); // creates a random number and stores it as the variable rnd
-                List<int> arrayList = new List<int>(); // Creates a new empty array(list) of numbers
-                for (int i = 0; i < numberOfDiceRolledInt; i++) // Rolling the dice 1 at a time until the full amount have been rolled
+                if (numberOfSidesPresentInt > 100 || numberOfDiceRolledInt > 100)
                 {
-                    int roll = rnd.Next(1, (numberOfSidesPresentInt + 1)); // returns a random value between 1 and the number of sides+1
-                    int totalRolled = roll; // sets the value the roll landed on
-                    arrayList.Add(totalRolled); // adds the value of the roll to the array
+                    await msg.Channel.SendMessageAsync("To avoid spam you cannot roll more than 100 dice or a d100 (Sorry)");
                 }
-                int total = arrayList.Sum(); // Sum of the array is defined here before I add text.
+                else if (numberOfSidesPresentInt == 1)
+                {
+                    await msg.Channel.SendMessageAsync("Grats you rolled a d1, hope you are proud");
+                }
+                else
+                {
+                    Random rnd = new Random(); // creates a random number and stores it as the variable rnd
+                    List<int> arrayList = new List<int>(); // Creates a new empty array(list) of numbers
+                    for (int i = 0; i < numberOfDiceRolledInt; i++) // Rolling the dice 1 at a time until the full amount have been rolled
+                    {
+                        int roll = rnd.Next(1, (numberOfSidesPresentInt + 1)); // returns a random value between 1 and the number of sides+1
+                        int totalRolled = roll; // sets the value the roll landed on
+                        arrayList.Add(totalRolled); // adds the value of the roll to the array
+                    }
+                    int total = arrayList.Sum(); // Sum of the array is defined here before I add text.
 
-                string rolls = string.Join(", ", arrayList);
+                    string rolls = string.Join(", ", arrayList);
 
-                await msg.Channel.SendMessageAsync("You Rolled:");
-                await msg.Channel.SendMessageAsync(rolls);
-                await msg.Channel.SendMessageAsync("Total: " + total.ToString());
+                    await msg.Channel.SendMessageAsync(DiscordID + " Rolled:");
+                    await msg.Channel.SendMessageAsync(rolls);
+                    await msg.Channel.SendMessageAsync("Total: " + total.ToString());
+                }
+            }
+            else
+            {
+                await msg.Channel.SendMessageAsync("Invalid Input. Roll a dice by using the format 'xdy' ");
             }
         }
     }
