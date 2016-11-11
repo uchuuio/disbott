6dd4f87e5b8457e70b7b7d8e17bf30730776e5a4
Samuel Rule <samuel.rule@reedonline.co.uk>
2016-11-11 15:06:02 +0000

    Fixed the giphy issue and created branch

---
+++ Disbott/Giphy/Gif.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disbott.Modules
+{
+    public class Gif
+    {
+        public Dictionary<string, string> data { get; set; }
+    }
+}
---
+++ Disbott/Objects/Gif.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disbott.Objects
+{
+    public class Gif
+    {
+        public Dictionary<string, string> data { get; set; }
+    }
+
+    public class GifSource
+    {
+        public string Type { get; set; }
+        public string Source { get; set; }
+    }
+}
