dafededc2f6bb4b699620148f52899eb31fbee44
80cmdude <s80cmdude@gmail.com>
2016-11-13 17:15:28 +0000

    Starting to sort out polling

---
+++ Disbott/Models/Objects/Polls.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disbott.Models.Objects
+{
+    public class Polls
+    {
+        public int Id { get; set; }
+        public string PollName { get; set; }
+        public int Yes { get; set; }
+        public int No { get; set; }
+    }
+}
---
+++ Disbott/Views/PollCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Timers;
+using Discord;
+using Discord.Commands;
+using Disbott.Models.Objects;
+using LiteDB;
+
+namespace Disbott.Views
+{
+    [Module]
+    public class PollCommand
+    {
+        Polls newPoll = new Polls()
+        {
+            PollName = "",
+            Yes = 0,
+            No = 0
+
+        };
+
+        [Command("newpoll"), Description("Starts A Poll")]
+        public async Task newpoll(IUserMessage msg, string pollName)
+        {
+            newPoll.PollName = pollName;
+            Timer timer = new Timer();
+            
+        }
+
+        [Command("vote"), Description("Vote on a poll")]
+        public async Task vote(IUserMessage msg, string vote)
+        {
+            
+        }
+    }
+}
