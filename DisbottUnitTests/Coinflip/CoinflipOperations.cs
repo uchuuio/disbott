75ae91f7b9fae9251400a4e591305ce32b443837
Thomas Moore <thomas.moore@reedonline.co.uk>
2016-12-09 16:01:50 +0000

    Add tests for coinflip

---
+++ DisbottUnitTests/Coinflip/CoinflipOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+using Disbott.Controllers;
+
+namespace DisbottUnitTests
+{
+    /// <summary>
+    /// Tests to confirm coinflips work as intended
+    /// </summary>
+    [TestFixture]
+    public class CoinflipOperations
+    {
+        /// <summary>
+        /// Validate that coin returns either Heads or Tails
+        /// </summary>
+        [Test]
+        public void Coin_Is_Either_Heads_Or_Tails()
+        {
+            var coinflip = CoinFlipController.Flip();
+            string[] possibleOutcomes = new string[] {"Heads", "Tails"};
+            Assert.Contains(coinflip, possibleOutcomes);
+        }
+    }
+}
