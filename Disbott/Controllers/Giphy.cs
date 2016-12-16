17a1587236a7695b93de71fbd5823bb49cde9116
Thomas Moore <thomas.moore@reedonline.co.uk>
2016-12-16 16:24:12 +0000

    Make giphy into new file structure #51

---
+++ Disbott/Controllers/Giphy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Disbott.Models.Objects;
+
+namespace Disbott.Controllers
+{
+    public static class GiphyController
+    {
+        public static async Task<string> GetRandomGif(string search)
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://api.giphy.com/v1/gifs/random");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            string result = null;
+            HttpResponseMessage response = await client.GetAsync($"?api_key=dc6zaTOxFJmzC&tag={search}");
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadAsStringAsync();
+            }
+
+            Gif gif = JsonConvert.DeserializeObject<Gif>(result);
+
+            return gif.data["image_url"];
+        }
+    }
+}
--- Disbott/Views/GiphyModule.cs
+++ Disbott/Views/GiphyModule.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 using Discord.Commands;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using Newtonsoft.Json;
-using Disbott.Models.Objects;
+
+using Disbott.Controllers;
 
 namespace Disbott.Views
 {
@@ -16,23 +13,7 @@ namespace Disbott.Views
         [Remarks("Gets a random gif")]
         public async Task Giphy([Remainder]string search = null)
         {
-            HttpClient client = new HttpClient();
-
-            client.BaseAddress = new Uri("http://api.giphy.com/v1/gifs/random");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            string result = null;
-            HttpResponseMessage response = await client.GetAsync($"?api_key=dc6zaTOxFJmzC&tag={search}");
-            if (response.IsSuccessStatusCode)
-            {
-                result = await response.Content.ReadAsStringAsync();
-            }
-
-            Gif gif = JsonConvert.DeserializeObject<Gif>(result);
-
-            var value = gif.data["image_url"];
-
+            var value = await GiphyController.GetRandomGif(search);
             await ReplyAsync(value);
         }
     }
