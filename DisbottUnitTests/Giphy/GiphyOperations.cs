76445110671469e5dd81943e3e630724697413e0
Thomas Moore <thomas.moore@reedonline.co.uk>
2016-12-16 16:42:12 +0000

    Tests for giphy module

---
+++ DisbottUnitTests/Giphy/GiphyOperations.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Disbott.Controllers;
+using NUnit.Framework;
+
+namespace DisbottUnitTests
+{
+    /// <summary>
+    /// Tests to confirm coinflips work as intended
+    /// </summary>
+    [TestFixture]
+    public class GiphyOperations
+    {
+        /// <summary>
+        /// Validate that giphy returns a gif
+        /// </summary>
+        [Test]
+        public async Task Gif_is_a_gif()
+        {
+            var gif = await GiphyController.GetRandomGif("test");
+            Assert.That(gif, Does.StartWith("http"));
+            Assert.That(gif, Does.EndWith("/giphy.gif"));
+        }
+    }
+}
