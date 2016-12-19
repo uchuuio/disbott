013c79190d894cbc16e78d0da8e76915cfec44b4
Samuel Rule <samuel.rule@reedonline.co.uk>
2016-12-19 15:05:48 +0000

    Initial srart of management module

---
+++ Disbott/Views/ManagementModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using LiteDB;
+using Discord.Commands;
+
+using Disbott.Controllers;
+using Disbott.Models.Objects;
+using Disbott.Properties;
+using Discord;
+
+namespace Disbott.Views
+{
+    [Name("Management")]
+    public class ManagementModule : ModuleBase
+    {
+        [Command("Kick")]
+        [Remarks("Kicks a user")]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        public async Task KickUser()
+        {
+            
+        }
+    }
+}
