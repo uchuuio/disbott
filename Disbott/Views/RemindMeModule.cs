06a754a74e39db3d1ac94eaa8355261c95070852
Sam Rule <s80cmdude@gmail.com>
2016-12-29 14:15:15 +0000

    Added a couple more commands and tests were successful

--- Disbott/Views/RemindMeModule.cs
+++ Disbott/Views/RemindMeModule.cs
@@ -13,37 +13,59 @@ namespace Disbott.Views
     public class RemindMeModule : ModuleBase
     {
         private System.Threading.Timer timer;
-        //public delegate void MyDel(string note);
-        //public event MyDel MyEvent;
-
-        [Command("datenote")]
-        [Remarks("adds a new note for a date")]
-        public async Task DateNote(int year, int month, int day, int hour, int minuite, int second, [Remainder]string newnote)
-        {
-            var msg = Context.Message;
-            var discordId = msg.Author.Username;
-
-            var currentTime = DateTime.Now;
-            var userTime = new DateTime(year, month, day, hour, minuite, second);
-            var timeToWait = userTime.Subtract(currentTime);
-            TimeSpan timeToGo = timeToWait;
-
-            await ReplyAsync($"{discordId} added a new note for {userTime.ToString()}");
-            await ReplyAsync($"time to wait is {timeToWait.ToString()}");
-
-            if (timeToGo < TimeSpan.Zero)
-            {
-               await ReplyAsync("Time Passed Fam");
-            }
-            this.timer = new System.Threading.Timer(x =>
-            {
-                this.ReplyWithNote($"@{discordId} {newnote} {userTime.ToString()} \r\n The time is currently {DateTime.Now.ToString()}");
-            }, null, timeToGo, Timeout.InfiniteTimeSpan);
-        }
-
         public async void ReplyWithNote(string note)
         {
             await ReplyAsync(note);
+        }
+
+        [Command("reminddatetime")]
+        [Remarks("adds a new note for a date")]
+        public async Task RemindDateTime(string date, [Remainder]string note)
+        {
+            var msg = Context.Message;
+            var discordId = msg.Author.Username;
+            try
+            {
+                var currentTime = DateTime.Now;
+                var userTime = DateTime.Parse(date);
+                var timeToWait = userTime.Subtract(currentTime);
+                TimeSpan timeToGo = timeToWait;
+
+                await ReplyAsync($"Don't worry {discordId}! I will remind you at {userTime}");
+
+                if (timeToGo < TimeSpan.Zero)
+                {
+                    await ReplyAsync("Time Passed Fam");
+                }
+                this.timer = new System.Threading.Timer(x =>
+                {
+                    this.ReplyWithNote($"{msg.Author.Mention} Remember: {note} \r\n This test was set up at {userTime} it is currently {DateTime.Now.ToString()}");
+                }, null, timeToGo, Timeout.InfiniteTimeSpan);
+            }
+            catch(FormatException e)
+            {
+                await ReplyAsync("Date was in an incorrect format. Use the format 'DD/MM/YYYY HH:MM:SS'");
+            }
+            catch(Exception e)
+            {
+                await ReplyAsync(e.Message);
+            }
+        }
+
+        [Command("remindme")]
+        [Remarks("Reminds you after a time")]
+        public async Task RemindMe(int hours, int mins , int seconds, [Remainder]string note)
+        {
+            var msg = Context.Message;
+            var discordId = msg.Author.Username;
+            TimeSpan timeToWait = new TimeSpan(hours, mins, seconds);
+            TimeSpan timeToGo = timeToWait;
+            await ReplyAsync($"Don't worry {discordId}! I will remind you in {timeToWait}");
+
+            this.timer = new System.Threading.Timer(x =>
+            {
+                this.ReplyWithNote($"{msg.Author.Mention} Remember: {note}");
+            }, null, timeToGo, Timeout.InfiniteTimeSpan);
         }
     }
 }
