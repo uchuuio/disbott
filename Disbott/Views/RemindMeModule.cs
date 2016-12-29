f99b115c869ffcc3a6dcf10e2699d75cb6230e97
Sam Rule <s80cmdude@gmail.com>
2016-12-29 14:41:38 +0000

    Added admin permissions to message all

--- Disbott/Views/RemindMeModule.cs
+++ Disbott/Views/RemindMeModule.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -66,6 +67,58 @@ namespace Disbott.Views
             {
                 this.ReplyWithNote($"{msg.Author.Mention} Remember: {note}");
             }, null, timeToGo, Timeout.InfiniteTimeSpan);
+        }
+
+        [Command("remindall")]
+        [Remarks("Reminds Everyone")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task RemindAll(int hours, int mins, int seconds, [Remainder]string note)
+        {
+            var msg = Context.Message;
+            var discordId = msg.Author.Username;
+            TimeSpan timeToWait = new TimeSpan(hours, mins, seconds);
+            TimeSpan timeToGo = timeToWait;
+            await ReplyAsync($"Yes sir {discordId}! I will remind everyone in {timeToWait}");
+
+            this.timer = new System.Threading.Timer(x =>
+            {
+                this.ReplyWithNote($"@everyone Remember: {note}");
+            }, null, timeToGo, Timeout.InfiniteTimeSpan);
+        }
+
+        [Command("remindalldatetime")]
+        [Remarks("adds a new note for a date")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task RemindAllDateTime(string date, [Remainder]string note)
+        {
+            var msg = Context.Message;
+            var discordId = msg.Author.Username;
+            try
+            {
+                var currentTime = DateTime.Now;
+                var userTime = DateTime.Parse(date);
+                var timeToWait = userTime.Subtract(currentTime);
+                TimeSpan timeToGo = timeToWait;
+
+                await ReplyAsync($"Yes Sir {discordId}! I will remind you at {userTime}");
+
+                if (timeToGo < TimeSpan.Zero)
+                {
+                    await ReplyAsync("Time Passed Fam");
+                }
+                this.timer = new System.Threading.Timer(x =>
+                {
+                    this.ReplyWithNote($"@everyone Remember: {note}");
+                }, null, timeToGo, Timeout.InfiniteTimeSpan);
+            }
+            catch (FormatException e)
+            {
+                await ReplyAsync("Date was in an incorrect format. Use the format 'DD/MM/YYYY HH:MM:SS'");
+            }
+            catch (Exception e)
+            {
+                await ReplyAsync(e.Message);
+            }
         }
     }
 }
