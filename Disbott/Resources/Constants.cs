0d762a4f0dbb3b57a8f1ad406dc5162e09a7a479
Thomas Moore <thomas.moore@reedonline.co.uk>
2017-01-05 08:59:45 +0000

    Should fix the tests on appveyor, constants -> Release

--- Disbott/Resources/Constants.cs
+++ Disbott/Resources/Constants.cs
@@ -9,25 +9,24 @@ namespace Disbott
 {
     public static class Constants
     {
-#if DEBUG
-        public static string quotePath = Path.GetDirectoryName(Path.GetDirectoryName
-            (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\quotestest.db";
+        #if DEBUG
+            public static string quotePath = Path.GetDirectoryName(Path.GetDirectoryName
+                (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\quotestest.db";
 
-        public static string remindMePath = Path.GetDirectoryName(Path.GetDirectoryName
-            (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\remindmetest.db";
+            public static string remindMePath = Path.GetDirectoryName(Path.GetDirectoryName
+                (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\remindmetest.db";
 
-        public static string pollPath = Path.GetDirectoryName(Path.GetDirectoryName
-            (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\polltest.db";
-#else
+            public static string pollPath = Path.GetDirectoryName(Path.GetDirectoryName
+                (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\polltest.db";
+        #else
+            public static string quotePath = Path.GetDirectoryName(Path.GetDirectoryName
+                (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Release\\quotes.db";
 
-        public static string quotePath = Path.GetDirectoryName(Path.GetDirectoryName
-           (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\quotes.db";
+            public static string remindMePath = Path.GetDirectoryName(Path.GetDirectoryName
+                (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Release\\remindme.db";
 
-        public static string remindMePath = Path.GetDirectoryName(Path.GetDirectoryName
-            (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\remindme.db";
-
-        public static string pollPath = Path.GetDirectoryName(Path.GetDirectoryName
-            (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\poll.db";
-#endif
-        }
+            public static string pollPath = Path.GetDirectoryName(Path.GetDirectoryName
+                (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Release\\poll.db";
+        #endif
+    }
 }
