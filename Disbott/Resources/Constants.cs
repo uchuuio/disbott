cc248c2d0075f91e224d278f5afbb0d4f7eb7fe9
Thomas Moore <thomas.moore@reedonline.co.uk>
2017-01-13 15:11:57 +0000

    Fixes alignment issue with the constants.cs file

--- Disbott/Resources/Constants.cs
+++ Disbott/Resources/Constants.cs
@@ -9,29 +9,28 @@ namespace Disbott
 {
     public static class Constants
     {
-#if DEBUG
-        //Database Paths
-        public static string quotePath = Path.GetDirectoryName(Path.GetDirectoryName
-            (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\quotestest.db";
-         public static string remindMePath = Path.GetDirectoryName(Path.GetDirectoryName
-            (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\remindmetest.db";
-         public static string pollPath = Path.GetDirectoryName(Path.GetDirectoryName
-            (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\polltest.db";
-        public static string MessageCountPath = Path.GetDirectoryName(Path.GetDirectoryName
-            (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\MessageCounttest.db";
+        #if DEBUG
+            //Database Paths
+            public static string quotePath = Path.GetDirectoryName(Path.GetDirectoryName
+                (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\quotestest.db";
+             public static string remindMePath = Path.GetDirectoryName(Path.GetDirectoryName
+                (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\remindmetest.db";
+             public static string pollPath = Path.GetDirectoryName(Path.GetDirectoryName
+                (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\polltest.db";
+            public static string MessageCountPath = Path.GetDirectoryName(Path.GetDirectoryName
+                (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase))).Remove(0, 6) + "\\bin\\Debug\\MessageCounttest.db";
 
-        //Image Paths
-        public static string avatarImage = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "/Resources/DISBOTTAvatarMH.png";
-#else
-        //Database Paths
-        public static string quotePath = "Quote.db";
-        public static string remindMePath = "remindMe.db";
-        public static string pollPath = "poll.db";
-        public static string MessageCountPath = "MessageCount.db";
+            //Image Paths
+            public static string avatarImage = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "/Resources/DISBOTTAvatarMH.png";
+        #else
+            //Database Paths
+            public static string quotePath = "Quote.db";
+            public static string remindMePath = "remindMe.db";
+            public static string pollPath = "poll.db";
+            public static string MessageCountPath = "MessageCount.db";
 
-        //Image Paths
-        //public static string avatarImage = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "/Resources/DisbottAVAV2.png";
-#endif
-
+            //Image Paths
+            //public static string avatarImage = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "/Resources/DisbottAVAV2.png";
+        #endif
     }
 }
