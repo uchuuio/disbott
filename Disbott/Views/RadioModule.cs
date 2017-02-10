af4e2fba71898a59c2e93e1904be819c75297636
Thomas Moore <thomas.moore@reedonline.co.uk>
2017-02-10 17:20:30 +0000

    Start work on radio module

---
+++ Disbott/Views/RadioModule.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using WebSocketSharp;
+
+using Discord.Commands;
+
+namespace Disbott.Views
+{
+    [Name("Radio")]
+    public class RadioModule : ModuleBase
+    {
+        [Command("request")]
+        [Remarks("Requests to queue the youtube video into the playlist")]
+        public async Task RequestSong()
+        {
+            var ws = new WebSocket("ws://localhost:3000");
+            ws.Connect();
+            ws.SendAsync();
+        }
+
+        [Command("current")]
+        [Remarks("Gets currently playing song")]
+        public async Task CurrentSong()
+        {
+
+        }
+
+        [Command("skip")]
+        [Remarks("Skips to the next song")]
+        public async Task SkipSong()
+        {
+
+        }
+
+        [Command("playlist")]
+        [Remarks("Tells user where to find the playlist")]
+        public async Task Playlist()
+        {
+
+        }
+    }
+}
